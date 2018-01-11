3121dc0a292222c0a12e8b04fe5a7da648945f1f
Mark Ayers <mark@philoserf.com>
2018-01-11 15:35:43 -0800

    For conversation

--- api/hello.pb.go
+++ api/hello.pb.go
@@ -36,7 +36,7 @@ const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 // *
 // Knock the door or not
 type Knock struct {
-	KnockDoor bool `protobuf:"varint,1,opt,name=knockDoor" json:"knockDoor,omitempty"`
+	Knocked bool `protobuf:"varint,1,opt,name=knocked" json:"knocked,omitempty"`
 }
 
 func (m *Knock) Reset()                    { *m = Knock{} }
@@ -44,9 +44,9 @@ func (m *Knock) String() string            { return proto.CompactTextString(m) }
 func (*Knock) ProtoMessage()               {}
 func (*Knock) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
-func (m *Knock) GetKnockDoor() bool {
+func (m *Knock) GetKnocked() bool {
 	if m != nil {
-		return m.KnockDoor
+		return m.Knocked
 	}
 	return false
 }
@@ -54,8 +54,8 @@ func (m *Knock) GetKnockDoor() bool {
 // *
 // Reply based on whether a door was knocked or not.
 type Reply struct {
-	Reply        bool   `protobuf:"varint,1,opt,name=reply" json:"reply,omitempty"`
-	ReplyMessage string `protobuf:"bytes,2,opt,name=replyMessage" json:"replyMessage,omitempty"`
+	Reply   bool   `protobuf:"varint,1,opt,name=reply" json:"reply,omitempty"`
+	Message string `protobuf:"bytes,2,opt,name=Message" json:"Message,omitempty"`
 }
 
 func (m *Reply) Reset()                    { *m = Reply{} }
@@ -70,9 +70,9 @@ func (m *Reply) GetReply() bool {
 	return false
 }
 
-func (m *Reply) GetReplyMessage() string {
+func (m *Reply) GetMessage() string {
 	if m != nil {
-		return m.ReplyMessage
+		return m.Message
 	}
 	return ""
 }
@@ -90,70 +90,70 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// Client API for HelloService service
+// Client API for Hello service
 
-type HelloServiceClient interface {
+type HelloClient interface {
 	// *
 	// Determines if you knocked the door and the appropriate
-	// reply if you didnt.
-	GetHello(ctx context.Context, in *Knock, opts ...grpc.CallOption) (*Reply, error)
+	// reply if you didn't.
+	Knocked(ctx context.Context, in *Knock, opts ...grpc.CallOption) (*Reply, error)
 }
 
-type helloServiceClient struct {
+type helloClient struct {
 	cc *grpc.ClientConn
 }
 
-func NewHelloServiceClient(cc *grpc.ClientConn) HelloServiceClient {
-	return &helloServiceClient{cc}
+func NewHelloClient(cc *grpc.ClientConn) HelloClient {
+	return &helloClient{cc}
 }
 
-func (c *helloServiceClient) GetHello(ctx context.Context, in *Knock, opts ...grpc.CallOption) (*Reply, error) {
+func (c *helloClient) Knocked(ctx context.Context, in *Knock, opts ...grpc.CallOption) (*Reply, error) {
 	out := new(Reply)
-	err := grpc.Invoke(ctx, "/hello.HelloService/GetHello", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/hello.Hello/Knocked", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// Server API for HelloService service
+// Server API for Hello service
 
-type HelloServiceServer interface {
+type HelloServer interface {
 	// *
 	// Determines if you knocked the door and the appropriate
-	// reply if you didnt.
-	GetHello(context.Context, *Knock) (*Reply, error)
+	// reply if you didn't.
+	Knocked(context.Context, *Knock) (*Reply, error)
 }
 
-func RegisterHelloServiceServer(s *grpc.Server, srv HelloServiceServer) {
-	s.RegisterService(&_HelloService_serviceDesc, srv)
+func RegisterHelloServer(s *grpc.Server, srv HelloServer) {
+	s.RegisterService(&_Hello_serviceDesc, srv)
 }
 
-func _HelloService_GetHello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Hello_Knocked_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Knock)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(HelloServiceServer).GetHello(ctx, in)
+		return srv.(HelloServer).Knocked(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/hello.HelloService/GetHello",
+		FullMethod: "/hello.Hello/Knocked",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HelloServiceServer).GetHello(ctx, req.(*Knock))
+		return srv.(HelloServer).Knocked(ctx, req.(*Knock))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _HelloService_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "hello.HelloService",
-	HandlerType: (*HelloServiceServer)(nil),
+var _Hello_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "hello.Hello",
+	HandlerType: (*HelloServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "GetHello",
-			Handler:    _HelloService_GetHello_Handler,
+			MethodName: "Knocked",
+			Handler:    _Hello_Knocked_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -163,15 +163,15 @@ var _HelloService_serviceDesc = grpc.ServiceDesc{
 func init() { proto.RegisterFile("hello.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 153 bytes of a gzipped FileDescriptorProto
+	// 150 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xce, 0x48, 0xcd, 0xc9,
-	0xc9, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x05, 0x73, 0x94, 0x54, 0xb9, 0x58, 0xbd,
-	0xf3, 0xf2, 0x93, 0xb3, 0x85, 0x64, 0xb8, 0x38, 0xb3, 0x41, 0x0c, 0x97, 0xfc, 0xfc, 0x22, 0x09,
-	0x46, 0x05, 0x46, 0x0d, 0x8e, 0x20, 0x84, 0x80, 0x92, 0x23, 0x17, 0x6b, 0x50, 0x6a, 0x41, 0x4e,
-	0xa5, 0x90, 0x08, 0x17, 0x6b, 0x11, 0x88, 0x01, 0x55, 0x02, 0xe1, 0x08, 0x29, 0x71, 0xf1, 0x80,
-	0x19, 0xbe, 0xa9, 0xc5, 0xc5, 0x89, 0xe9, 0xa9, 0x12, 0x4c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41, 0x28,
-	0x62, 0x46, 0x16, 0x5c, 0x3c, 0x1e, 0x20, 0x2b, 0x83, 0x53, 0x8b, 0xca, 0x32, 0x93, 0x53, 0x85,
-	0x34, 0xb8, 0x38, 0xdc, 0x53, 0x4b, 0xc0, 0x42, 0x42, 0x3c, 0x7a, 0x10, 0xa7, 0x81, 0x9d, 0x22,
-	0x05, 0xe3, 0x81, 0x6d, 0x54, 0x62, 0x48, 0x62, 0x03, 0xbb, 0xd8, 0x18, 0x10, 0x00, 0x00, 0xff,
-	0xff, 0x3a, 0xff, 0x1a, 0x47, 0xc0, 0x00, 0x00, 0x00,
+	0xc9, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x05, 0x73, 0x94, 0xd4, 0xb9, 0x58, 0xbd,
+	0xf3, 0xf2, 0x93, 0xb3, 0x85, 0x24, 0xb8, 0xd8, 0xb3, 0x41, 0x8c, 0xd4, 0x14, 0x09, 0x46, 0x05,
+	0x46, 0x0d, 0x8e, 0x20, 0x18, 0xd7, 0x8b, 0x85, 0x83, 0x49, 0x80, 0x59, 0xc9, 0x96, 0x8b, 0x35,
+	0x28, 0xb5, 0x20, 0xa7, 0x52, 0x48, 0x84, 0x8b, 0xb5, 0x08, 0xc4, 0x80, 0x2a, 0x83, 0x70, 0x40,
+	0xda, 0x7d, 0x53, 0x8b, 0x8b, 0x13, 0xd3, 0x53, 0x25, 0x98, 0x14, 0x18, 0x35, 0x38, 0x83, 0x60,
+	0x5c, 0x2f, 0x16, 0x0e, 0x66, 0x01, 0x16, 0x23, 0x03, 0x2e, 0x56, 0x0f, 0x90, 0x85, 0x42, 0xea,
+	0x5c, 0xec, 0xde, 0x10, 0x83, 0x85, 0x78, 0xf4, 0x20, 0x0e, 0x02, 0xf3, 0xa5, 0x60, 0x3c, 0xb0,
+	0x2d, 0x4a, 0x0c, 0x49, 0x6c, 0x60, 0x77, 0x1a, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x82, 0x97,
+	0x43, 0x45, 0xb6, 0x00, 0x00, 0x00,
 }
--- server/server.go
+++ server/server.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
-    "os"
-    "os/signal"
-    "syscall"
-    "time"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	a "github.com/alejandroEsc/grpc-example/api"
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
 var (
-	port         = flag.Int("port", 8501, "Server port.")
-	knockFailure = flag.String("no-knock-message", "You should try and knock", "Failed to knock message.")
+	port = flag.Int(
+		"port",
+		8501,
+		"Server port.")
+	knockFailure = flag.String(
+		"no-knock-message",
+		"You should try and knock",
+		"Failed to knock message.")
 )
 
 type doorServer struct {
@@ -25,15 +31,15 @@ type doorServer struct {
 }
 
 // Implemented the server interface
-func (s *doorServer) GetHello(c context.Context, knock *a.Knock) (*a.Reply, error) {
+func (s *doorServer) GetHello(c context.Context, k *a.Knock) (*a.Reply, error) {
 
-	if knock == nil {
-		return nil, fmt.Errorf("nothing received, wont respond.")
+	if k == nil {
+		return nil, fmt.Errorf("nothing received, wont respond")
 	}
 
 	r := a.Reply{Reply: false, ReplyMessage: s.knockFailureMsg}
 
-	if knock.KnockDoor {
+	if k.KnockDoor {
 		r.Reply = true
 		r.ReplyMessage = "Hello!"
 	}
@@ -47,10 +53,9 @@ func newDoorServer(noKnockMsg string) *doorServer {
 }
 
 func main() {
-    var err error
-    log.Print("starting server")
+	log.Print("starting server")
 
-    // Parse arguments here
+	// Parse arguments here
 	flag.Parse()
 
 	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
@@ -63,30 +68,28 @@ func main() {
 	grpcServer := grpc.NewServer(opts...)
 	a.RegisterHelloServiceServer(grpcServer, newDoorServer(*knockFailure))
 
-    log.Printf("attempting to start server in port %d", *port)
+	log.Printf("attempting to start server in port %d", *port)
 
-    //  Get notified that server is being asked to stop
-    // Handle SIGINT and SIGTERM.
-    gracefulStop := make(chan os.Signal)
-    signal.Notify(gracefulStop, syscall.SIGINT, syscall.SIGTERM)
+	// Get notified that server is being asked to stop
+	// Handle SIGINT and SIGTERM.
+	gracefulStop := make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGINT, syscall.SIGTERM)
 
-    // Chance here to gracefully handle being stopped.
-    go func() {
-        sig := <-gracefulStop
-        log.Printf("caught sig: %+v", sig)
-        log.Println("Wait for 2 second to finish processing")
-        time.Sleep(2*time.Second)
-        grpcServer.Stop()
-        log.Print("service terminated")
-        os.Exit(0)
-    }()
+	// Chance here to gracefully handle being stopped.
+	go func() {
+		sig := <-gracefulStop
+		log.Printf("caught sig: %+v", sig)
+		log.Println("Wait for 2 second to finish processing")
+		time.Sleep(2 * time.Second)
+		grpcServer.Stop()
+		log.Print("service terminated")
+		os.Exit(0)
+	}()
 
-    err = grpcServer.Serve(listener)
+	err = grpcServer.Serve(listener)
 
-    if err != nil {
-        log.Fatalf("could not start service: ", err)
-    }
+	if err != nil {
+		log.Fatalf("could not start service: %v", err)
+	}
 
 }
-
-
